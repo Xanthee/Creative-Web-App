d61606fa4629824c9544a59e0918e15e588f083d
Xanthe <116354535+Xanthee@users.noreply.github.com>
2024-11-17 15:57:52 +0000

    creating the UI of the login page
    making the login UI and adding layout as an element that can be used on whatever pages i want a header and possible footer to be on

--- cafe-app/src/pages/LoginPage.jsx
+++ cafe-app/src/pages/LoginPage.jsx
@@ -1,4 +1,15 @@
 export default function LoginPage(){
-    return 'login form should go here';
-
+    return (
+        <div className="m-12">
+            <div className="flex flex-col">
+                <span className="font-bold text-2xl pb-4">Sign in</span>
+                <span className="text-sm text-slate-500 pb-4">Required fields are marked with an asterisk*</span>
+            </div>
+            <form className="flex flex-col ">
+                <input type="email" placeholder="*Enter your email" className="w-96 border border-slate-500 rounded-md p-2 mb-6"/>
+                <input type="password" placeholder="*Enter password" className="w-96 border border-slate-500 rounded-md p-2"/>
+                <button className=" w-24 font-bold text-l m-2 mt-10 p-3 bg-black text-white border border-black rounded-full">Login</button>
+            </form>
+        </div>
+    );
 }
