432cefd0300b4b0fd36e8ed9aacd6f495d277f76
Xanthe <116354535+Xanthee@users.noreply.github.com>
2024-11-24 16:34:25 +0000

    adding some front end to the main screen

--- cafe-app/src/pages/IndexPage.jsx
+++ cafe-app/src/pages/IndexPage.jsx
@@ -5,12 +5,47 @@ import coffeeMachineImg from '../assets/coffeeMachine.png';
 
 export default function IndexPage() {
     return (
-    <div className="flex place-content-center w-[70rem] h-[22rem] mt-10">
-        <div className="flex flex-row bg-green-900 w-4/5 items-center border rounded-lg">
-            <img src={coffeeMachineImg} alt="Coffee Machine Image" height={400} width={400} className="ml-5 border border-transparent rounded-lg"/>
-            <span className="text-white pl-4 text-2xl">Sign up today and get a free drink on us!</span>
-            <Link to={'/register'} className="font-bold underline">Register</Link>
+
+    <div className="flex flex-col w-auto h-auto p-3">
+        <div className="flex justify-center mt-10">
+
+            <div className="flex flex-row bg-green-900 items-center border rounded-lg">
+                <img src={coffeeMachineImg} alt="Coffee Machine Image" height={400} width={400} className="border border-transparent p-3 rounded-lg"/>
+                <div className="flex flex-col p-4">
+                    <span className="text-white text-2xl pb-6">Sign up today and get a free drink on us!</span>
+                    <Link to={'/register'} className="text-2xl font-bold text-center text-white underline">Register</Link>
+                </div>
+            </div>
         </div>
+
+        <div className="flex justify-center m-5">
+            <span className="text-2xl font-bold">Say hello to easy ordering, endless choice and - yes, free coffee.</span>
+        </div>
+
+
+            <div className="flex justify-center">
+                <div className="flex flex-row m-6">
+                    <div className="m-2">
+                        <img src={coffeeMachineImg} alt="Coffee Machine Image" height={120} width={120} className="border border-transparent rounded-full"/>  
+                        <h1>A</h1>
+                        <p>hello world</p>
+                    </div>
+                    <div className="m-2">
+                        <img src={coffeeMachineImg} alt="Coffee Machine Image" height={120} width={120} className="border border-transparent rounded-full"/>
+                        <h1>B</h1>
+                        <p>hello world</p>
+                    </div>
+                    <div className="m-2">
+                        <img src={coffeeMachineImg} alt="Coffee Machine Image" height={120} width={120} className="border border-transparent rounded-full"/>  
+                        <div className="">
+                            <h1>C</h1>
+                            <p>hello world</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
     </div>
+
     );
 }
