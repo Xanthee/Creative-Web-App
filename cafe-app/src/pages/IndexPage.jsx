32f35c6a43a8d45b347661686db72cb33091cd34
Xanthe <116354535+Xanthee@users.noreply.github.com>
2024-12-05 13:00:11 +0000

    css update

--- cafe-app/src/pages/IndexPage.jsx
+++ cafe-app/src/pages/IndexPage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import Header from "../Header";
 import React from 'react';
 import coffeeMachineImg from '../assets/coffeeMachine.png';
-
+import "../index.css";
 export default function IndexPage() {
     return (
 
@@ -22,12 +22,10 @@ export default function IndexPage() {
             <span className="text-2xl font-bold">Say hello to easy ordering, endless choice and - yes, free coffee.</span>
         </div>
 
-
-        <div className="flex justify-center" >
-            <div className="flex flex-row m-6">
+        <div className="flex flex-row justify-evenly pt-10 pb-10" >
                 <div className="m-2">
                     <img src={coffeeMachineImg} alt="Coffee Machine Image" height={120} width={120} className="border border-transparent rounded-full"/>  
-                    <h1>A</h1>
+                    <h1 className="flex items-center">A</h1>
                     <p>hello world</p>
                 </div>
                 <div className="m-2">
@@ -37,21 +35,21 @@ export default function IndexPage() {
                 </div>
                 <div className="m-2">
                     <img src={coffeeMachineImg} alt="Coffee Machine Image" height={120} width={120} className="border border-transparent rounded-full"/>  
-                    <div className="">
-                        <h1>C</h1>
-                        <p>hello world</p>
-                    </div>
+                    <h1>C</h1>
+                    <p>hello world</p>
                 </div>
-            </div>
         </div>
 
-        <div className="flex ">
 
-            <div className="flex col bg-green-900 items-center border rounded-lg w-full">
-                <span className="text-white text-2xl p-6">Sign up today and get a free drink on us!</span>
-                <img src={coffeeMachineImg} alt="Coffee Machine Image" height={400} width={400} className="border border-transparent p-3 rounded-lg"/>
+
+            <div className="flex row bg-green-900 items-center border rounded-lg w-full">
+                <div className="p-10">
+                    <h1 className="pb-8 text-white text-5xl">Lorem ipsum</h1>
+                    <p className="text-white text-2xl ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <img src={coffeeMachineImg} alt="Coffee Machine Image" height={550} width={550} className="border border-transparent p-3 rounded-lg flex flex-row-reverse "/>
             </div>
-        </div>
+
         
 
 
