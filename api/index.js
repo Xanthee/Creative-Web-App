02d3d577820c0a4c54a882a247c9a0ba33bdf3d3
Xanthe <116354535+Xanthee@users.noreply.github.com>
2024-12-09 20:39:12 +0000

    backend database

--- api/index.js
+++ api/index.js
@@ -1,18 +1,46 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const mongoose = require('mongoose');
+const { ServerMonitoringMode } = require('mongodb');
+require('dotenv').config();
 
-app.use(express.json());
-app.use(cors({
-    credentials: true,
-    origin: '127.0.0.1:3000', 
-}));
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(async () => {
+        console.log('db connected');
+        const app = express();
 
+        // Define a Schema
+        const userSchema = new mongoose.Schema({
+            email: { type: String, required: true },
+            username: { type: String, required: true },
+            password: { type: String, required: true },
+        });
+        app.use(cors({
+            credentials: true,
+            origin: 'http://127.0.0.1:3000',
+        }));
+        
+        // Define a Model
+        const userInfo = mongoose.model('userInfo', userSchema);
+        //route
+        app.post('/register',async(req, res) => {
+            try {
+                const {username, email, password} = req.body;
+                const newUser = new userInfo({username, email, password})
+                const savedUser = await newUser.save();
+                console.log(savedUser);
+                //onst userInfo = new Message({ content: "hi" });
+               // await message.save();
+               // console.log("Static message saved to database:", message);
+            } catch (err) {
+               // console.error("Error saving static message:", err.message);
+               console.error(err.savedUser);
+            }
+        });
+        
 
-
-app.post('/register', (req,res) => {
-    const {name,email,password} = req.body;
-    res.json({name,email,password});
-})
-
-app.listen(3000);
+        app.listen(5000, () => {
+            console.log('Server running on http://127.0.0.1:5000');
+        });
+    });    
--- cafe-app/src/pages/RegisterPage.jsx
+++ cafe-app/src/pages/RegisterPage.jsx
@@ -1,19 +1,28 @@
 import { Link } from "react-router-dom"
 import axios from "axios";
-import {useState} from "react";
+import { useState } from "react";
 
-export default function RegisterPage(){
-    const [name,setName] = useState('');
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
-    function registerUser(ev) {
-        ev.preventDefault();
+export default function RegisterPage() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    
+    function registerUser(e) {
+        e.preventDefault();
         axios.post('/register', {
-            name,
+            username,
             email,
             password,
-        });
+        })
+            .then(result => console.log(result))
+            .catch(err => console.log(err));
     }
+
+
+
+
 
     return (
         <div className="m-12">
@@ -22,17 +31,36 @@ export default function RegisterPage(){
                 <span className="text-sm text-slate-500 pb-4">Required fields are marked with an asterisk*</span>
             </div>
             <form className="flex flex-col " onSubmit={registerUser}>
-                <input type="name" placeholder="*Enter your name" className="mb-6" 
-                    value={name} onChange={ev => setName(ev.target.value)} />
-                <input type="email" placeholder="*Enter your email" className="mb-6"
-                    value={email} onChange={ev => setEmail(ev.target.value)} />
-                <input type="password" placeholder="*Enter password"
-                    value={password} onChange={ev => setPassword(ev.target.value)} />
+
+                <input
+                    type="name"
+                    placeholder="*Enter your name"
+                    className="mb-6"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+
+                <input
+                    type="email"
+                    placeholder="*Enter your email"
+                    className="mb-6"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+
+                <input
+                    type="password"
+                    placeholder="*Enter password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+
                 <button className=" w-24 font-bold text-l m-2 mt-10 p-3 bg-black text-white border border-black rounded-full">Sign up</button>
-                <div className="">  
-                    Already a member? <Link to={'/login'} className="font-bold underline">login</Link>                    
+                <div className="">
+                    Already a member? <Link to={'/login'} className="font-bold underline">login</Link>
                 </div>
             </form>
         </div>
     );
 }
+
